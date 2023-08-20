8ba0850e44b9069de416fea787bf4015017e97d7
roylee0912 <leeroy0912@gmail.com>
2023-08-19 23:56:06 -0700

    added bikeshare data based on lyft api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,12 +8,16 @@ import {AiOutlineShoppingCart} from "react-icons/ai"
 import {PiPersonSimpleBikeBold} from "react-icons/pi"
 import {CiDumbbell} from "react-icons/ci"
 import {LiaBusSolid} from "react-icons/lia"
+import {SearchBox} from '@mapbox/search-js-react';
+
+
 mapboxgl.accessToken = process.env.REACT_APP_TOKEN;
 
 const Home = () => {
   const mapContainer = useRef(null);
   const map = useRef(null);
   const parkMarkers = useRef([]); 
+  const locationMarker=useRef(null);
 
   const [mapLoaded, setMapLoaded] = useState(false);
 
@@ -25,7 +29,20 @@ const Home = () => {
   const [showQuadrantColors,setShowQuadrantColors]=useState(false)
   const [showParks,setShowParks]=useState(false);
   const [showCrime,setShowCrime]=useState(false);
-  
+  const [showLocation,setShowLocation]=useState(false);
+    const [locationLatLong,setLocationLatLong]=useState(null)
+    const [locationName,setLocationName]=useState("")
+  const [showGrocery,setShowGrocery]=useState(false);
+  //groceries
+    const [traderJoes,setTraderJoes]=useState(false);
+    const [wholeFoods,setWholeFoods]=useState(false);
+    const [safeway,setSafeway]=useState(false);
+
+  const [showBart,setShowBart]=useState(false);
+  const [showBikes,setShowBikes]=useState(false);
+  const [showGyms,setShowGyms]=useState(false);
+  const [showBuses,setShowBuses]=useState(false);
+
 
   const updateOpacity = () => {
     if (!map.current) return;
@@ -42,6 +59,11 @@ const Home = () => {
     if (showCrime) {
       opacityExpression = ['-', opacityExpression, ['*', ['get', 'crime_score'], 0.1]];
     }
+    // Add bike_score based opacity if showBikes is true
+    if (showBikes) {
+      opacityExpression = ['-', opacityExpression, ['*', ['get', 'bike_score'], 0.1]];
+    }
+
   
     // Ensure opacity doesn't exceed 1
     opacityExpression = ['min', 1, opacityExpression];
@@ -54,35 +76,35 @@ const Home = () => {
 
   //FUNCTION TO SHOW PARKS
   // Function to toggle park markers
-  //   const toggleParkMarkers = (show) => {
-  //     if (show) {
-  //       fetch('/static/media/parks.157b43c98b996f8882ca.geojson')
-  //       .then((r) => r.json())
-  //       .then((data) => {
-  //         data.features.forEach((feature) => {
-  //           // Create markers using feature properties
-  //           const popup = new mapboxgl.Popup({ offset: 25 }).setText(
-  //             feature.properties.Map_Label
-  //           );
-  //           const { coordinates } = feature.geometry;
-  //           const marker = new mapboxgl.Marker({
-  //             color: '#57fa7d',
-  //             scale: 0.6,
-  //           })
-  //             .setLngLat(coordinates)
-  //             .setPopup(popup)
-  //             .addTo(map.current);
+    const toggleParkMarkers = (show) => {
+      if (show) {
+        fetch('/static/media/parks.157b43c98b996f8882ca.geojson')
+        .then((r) => r.json())
+        .then((data) => {
+          data.features.forEach((feature) => {
+            // Create markers using feature properties
+            const popup = new mapboxgl.Popup({ offset: 25 }).setText(
+              feature.properties.Map_Label
+            );
+            const { coordinates } = feature.geometry;
+            const marker = new mapboxgl.Marker({
+              color: '#57fa7d',
+              scale: 0.6,
+            })
+              .setLngLat(coordinates)
+              .setPopup(popup)
+              .addTo(map.current);
   
-  //           // Add the created marker to the parkMarkers array
-  //           parkMarkers.current.push(marker);
-  //         });
-  //       });
-  //   } else {
-  //     // Remove existing markers if show is false
-  //     parkMarkers.current.forEach((marker) => marker.remove());
-  //     parkMarkers.current = [];
-  //   }
-  // };
+            // Add the created marker to the parkMarkers array
+            parkMarkers.current.push(marker);
+          });
+        });
+    } else {
+      // Remove existing markers if show is false
+      parkMarkers.current.forEach((marker) => marker.remove());
+      parkMarkers.current = [];
+    }
+  };
 
 
 
@@ -114,7 +136,7 @@ const Home = () => {
         type: 'fill',
         source: 'neighborhoods',
         paint: {
-          'fill-color': 'transparent'
+          'fill-color': 'transparent',
         },
       });
 
@@ -136,8 +158,7 @@ const Home = () => {
         type: 'fill',
         source: 'neighborhoods',
         paint: {
-          'fill-color': 'blue',
-          'fill-opacity': 0.8,
+          'fill-color': 'rgba(0,51,204,0.7)'
         },
       });
 
@@ -224,12 +245,6 @@ const Home = () => {
 
     });
 
-
-
-
-
-
-
     //UPDATE ZOOM AND CENTER OF MAP BASED ON DRAG
       map.current.on('move', () => {
         setZoom(map.current.getZoom().toFixed(2));
@@ -249,31 +264,49 @@ const Home = () => {
       setMapLoaded(true);
   },[]);
 
-  useEffect(() => {
-    //SECOND USE EFFECT TO UPDATE COLORS WITHOUT RERENDER
-    if (map.current && mapLoaded) {
-      // toggleParkMarkers(showParks);
-      
 
-      updateOpacity(showParks, showCrime);
+
+
+
+
+//SECOND USE EFFECT TO UPDATE THINGS WITHOUT RERENDER
+
+  useEffect(() => {
+    if (map.current && mapLoaded) {
+    
+      updateOpacity(showParks, showCrime, showBikes);
   
 
       
       map.current.setPaintProperty('quadrant-fill', 'fill-color', showQuadrantColors
-        ? [
-            'match',
-            ['get', 'quad'],
-            'SE', 'lightgreen',
-            'SW', 'orange',
-            'NE', 'blue',
-            'NW', 'red',
-            'blue' // Default value if no match
-          ]
-        : 'blue' // static color fill
-      );
+      ? [
+          'match',
+          ['get', 'quad'],
+          'SE', 'rgba(144, 238, 144, 0.7)', // lightgreen with 0.7 opacity
+          'SW', 'rgba(255, 165, 0, 0.7)',    // orange with 0.7 opacity
+          'NE', 'rgba(0,51,204,0.7)',  // blue with 0.7 opacity
+          'NW', 'rgba(255, 0, 0, 0.7)',      // red with 0.7 opacity
+          'rgba(0,51,204,0.7)'         // Default value if no match, blue with 0.7 opacity
+        ]
+      : 'rgba(0,51,204,0.7)' // static color fill with 0.7 opacity
+    );
 
-    }
-  }, [showQuadrantColors, showParks, showCrime, map.current]);
+      if (locationLatLong) {
+        // Remove existing marker if any
+        if (locationMarker.current) {
+          locationMarker.current.remove();
+        }
+      
+        // Create a new marker at the location
+        locationMarker.current = new mapboxgl.Marker()
+          .setLngLat(locationLatLong)
+          .addTo(map.current);
+      }
+      }
+
+      
+
+  }, [showQuadrantColors, showParks, showCrime, showBikes, locationLatLong, map.current]);
 
 
 
@@ -308,39 +341,45 @@ const Home = () => {
         <div className="flex flex-col py-4 px-3">
           <h1 className="text-2xl text-gray-900 font-semibold">Add More Filters</h1>
           <h3 className="text-lg text-gray-600">Continue refining your ideal neighborhoods</h3>
-          <div className="flex flex-col border-b-4 border-black">
+          <div className="flex flex-col border-b-4 border-blue-900">
             <div className="flex my-2">
-              <div className="whitespace-nowrap py-2 px-3 border-2 border-gray-300 rounded-lg flex items-center cursor-pointer hover:bg-gray-100 duration-200 mr-2">
+              <div onClick={()=>setShowLocation(true)}
+                className={`w-fit whitespace-nowrap py-2 px-3 border-2 border-gray-300 ${showLocation ? "bg-gray-100 cursor-not-allowed opacity-50" : "opacity-100 hover:bg-gray-100 bg-white cursor-pointer"} rounded-lg flex items-center   duration-200 mr-2`}>
                 <GrLocation className="w-4 h-4 mx-1"/>Proximity to Location
               </div>
-              <div onClick={()=>setShowCrime(!showCrime)}
-              className="whitespace-nowrap py-2 px-3 border-2 border-gray-300 rounded-lg flex items-center cursor-pointer hover:bg-gray-100 duration-200">
+              <div onClick={()=>setShowCrime(true)}
+              className={`w-fit whitespace-nowrap py-2 px-3 border-2 border-gray-300 ${showCrime ? "bg-gray-100 cursor-not-allowed opacity-50" : "opacity-100 hover:bg-gray-100 bg-white cursor-pointer"} rounded-lg flex items-center   duration-200`}>
                 <BsShield className="w-4 h-4 mx-1"/>Safety
               </div>
             </div>
             <div className="flex mb-2">
-              <div onClick={()=>setShowParks(!showParks)}
-                    className="whitespace-nowrap py-2 px-3 border-2 border-gray-300 rounded-lg flex items-center cursor-pointer hover:bg-gray-100 duration-200 mr-2">
+              <div onClick={()=>setShowParks(true)}
+                className={`w-fit whitespace-nowrap py-2 px-3 border-2 border-gray-300 ${showParks ? "bg-gray-100 cursor-not-allowed opacity-50" : "opacity-100 hover:bg-gray-100 bg-white cursor-pointer"} rounded-lg flex items-center   duration-200 mr-2`}>
                 <BsTree className="w-4 h-4 mx-1"/>Parks and Rec Centers
               </div>
-              <div className="whitespace-nowrap py-2 px-3 border-2 border-gray-300 rounded-lg flex items-center cursor-pointer hover:bg-gray-100 duration-200">
-                <AiOutlineShoppingCart className="w-4 h-4 mx-1"/>Grocery chains
+              <div onClick={()=>setShowGrocery(true)}
+              className={`w-fit whitespace-nowrap py-2 px-3 border-2 border-gray-300 ${showGrocery ? "bg-gray-100 cursor-not-allowed opacity-50" : "opacity-100 hover:bg-gray-100 bg-white cursor-pointer"} rounded-lg flex items-center   duration-200`}>
+                <AiOutlineShoppingCart className="w-4 h-4 mx-1"/>Grocery Chains
               </div>
             </div>
             <div className="flex mb-2">
-              <div className="whitespace-nowrap py-2 px-3 border-2 border-gray-300 rounded-lg flex items-center cursor-pointer hover:bg-gray-100 duration-200 mr-2">
+              <div onClick={()=>setShowBart(true)}
+                className={`w-fit whitespace-nowrap py-2 px-3 border-2 border-gray-300 ${showBart ? "bg-gray-100 cursor-not-allowed opacity-50" : "opacity-100 hover:bg-gray-100 bg-white cursor-pointer"} rounded-lg flex items-center   duration-200 mr-2`}>
                 <BsTrainFront className="w-4 h-4 mx-1"/>BART Stations
               </div>
-              <div className="whitespace-nowrap py-2 px-3 border-2 border-gray-300 rounded-lg flex items-center cursor-pointer hover:bg-gray-100 duration-200">
-                <PiPersonSimpleBikeBold className="w-4 h-4 mx-1"/>Bikes
+              <div onClick={()=>setShowBikes(true)}
+              className={`w-fit whitespace-nowrap py-2 px-3 border-2 border-gray-300 ${showBikes ? "bg-gray-100 cursor-not-allowed opacity-50" : "opacity-100 hover:bg-gray-100 bg-white cursor-pointer"} rounded-lg flex items-center   duration-200`}>
+                <PiPersonSimpleBikeBold className="w-4 h-4 mx-1"/>BikeShare
               </div>
             </div>
             <div className="flex mb-2">
-              <div className="whitespace-nowrap py-2 px-3 border-2 border-gray-200 rounded-lg flex items-center cursor-pointer hover:bg-gray-100 duration-200 mr-2">
-                <CiDumbbell className="w-6 h-6 mx-1"/>Gym chains
+              <div onClick={()=>setShowGyms(true)}
+                className={`w-fit whitespace-nowrap py-2 px-3 border-2 border-gray-300 ${showGyms ? "bg-gray-100 cursor-not-allowed opacity-50" : "opacity-100 hover:bg-gray-100 bg-white cursor-pointer"} rounded-lg flex items-center   duration-200 mr-2`}>
+                <CiDumbbell className="w-4 h-4 mx-1"/>Gym Chains
               </div>
-              <div className="whitespace-nowrap py-2 px-3 border-2 border-gray-200 rounded-lg flex items-center cursor-pointer hover:bg-gray-100 duration-200">
-                <LiaBusSolid className="w-6 h-6 mx-1"/>Bus stops
+              <div onClick={()=>setShowBuses(true)}
+              className={`w-fit whitespace-nowrap py-2 px-3 border-2 border-gray-300 ${showBuses ? "bg-gray-100 cursor-not-allowed opacity-50" : "opacity-100 hover:bg-gray-100 bg-white cursor-pointer"} rounded-lg flex items-center   duration-200`}>
+                <LiaBusSolid className="w-4 h-4 mx-1"/>Bus Stops
               </div>
             </div>
           </div>
@@ -348,14 +387,17 @@ const Home = () => {
             <h1 className='text-2xl text-gray-900 font-semibold'>
               Filters
             </h1>
-            <h2>
-
+            <h2 className="text-gray-600">
+              Hold and drag here to delete
             </h2>
+            <div>
+              {displayActiveFilters()}
+            </div>
           </div>
         </div>
         
       </div>
-      <div className="w-full">
+      <div className="" style={{width: "100%"}}>
         <div className="absolute top-2 right-2 z-10 flex flex-col">
           <button className="text-xl font-extrabold text-black rounded-t-xl border-2 shadow-2xl  bg-white hover:bg-gray-200 duration-200 p-1 w-9 h-10" onClick={handleZoomIn}>
             +
@@ -372,11 +414,145 @@ const Home = () => {
       
         </div>
         
-        <div ref={mapContainer} className="absolute top-0" style={{ height: '100%', width:"80%" }} />
+        <div ref={mapContainer} className="top-0 h-full w-full" />
       </div>
 
     </div>
   );
+
+  function displayActiveFilters(){
+    return (
+    <div className="flex flex-col border-b-4 border-blue-900">
+        {
+          showLocation ?
+            <div onClick={()=>setShowLocation(false)}
+              className={`w-full whitespace-nowrap  rounded-lg flex flex-col duration-200 mr-2 cursor-pointer`}>
+               
+               <div onClick={(e)=>e.stopPropagation()} className="flex items-center">
+               <GrLocation className="w-4 h-4 mx-1"/>Proximity to Location
+                </div>
+               <div  onClick={(e)=>e.stopPropagation()} className="">
+                  <SearchBox accessToken={process.env.REACT_APP_TOKEN} 
+                            placeholder="Search"
+                            value={locationName.length>0 ? locationName : ""}
+                            bbox={[-122.537384,37.702566,-122.353363,37.810055]}
+                            onRetrieve={(e)=>{
+                                      setLocationName(e.features[0].properties.name)
+                                      setLocationLatLong(e.features[0].geometry.coordinates)}}
+                            className=""/>
+                </div>
+            </div>
+          :
+            <></>
+        }
+        {
+          showCrime ?
+            <div onClick={()=>setShowCrime(false)}
+              className={`w-full whitespace-nowrap  rounded-lg flex items-center duration-200 mr-2 cursor-pointer`}>
+              <BsShield className="w-4 h-4 mx-1"/>Safety
+            </div>
+          :
+            <></>
+        }
+        {
+          showParks ?
+            <div onClick={()=>setShowParks(false)}
+              className={`w-full whitespace-nowrap  rounded-lg flex items-center duration-200 mr-2 cursor-pointer`}>
+              <BsTree className="w-4 h-4 mx-1"/>Parks and Rec Centers
+            </div>
+          :
+            <></>
+        }
+        {
+          showGrocery ?
+            <div onClick={()=>setShowGrocery(false)}
+              className={`w-full whitespace-nowrap  rounded-lg flex flex-col duration-200 mr-2 cursor-pointer`}>
+               
+               <div className="flex items-center">
+                <AiOutlineShoppingCart className="w-4 h-4 mx-1"/>Grocery Chains
+                </div>
+               <div className="grid grid-cols-3 gap-2">
+                  <div onClick={(e)=>{e.stopPropagation()
+                                      setTraderJoes(!traderJoes)}} 
+                      className={`${traderJoes ? "bg-blue-500 border-blue-200" : "bg-white border-gray-500" } duration-100 text-center border-2 rounded-lg px-2 py-1 cursor-pointer`}>
+                    Trader Joe's
+                  </div>
+                  <div onClick={(e)=>{e.stopPropagation()
+                                      setWholeFoods(!wholeFoods)}} 
+                      className={`${wholeFoods ? "bg-blue-500 border-blue-200" : "bg-white border-gray-500" } duration-100 text-center border-2 rounded-lg px-2 py-1 cursor-pointer`}>
+                    Whole Foods
+                  </div>
+                  <div onClick={(e)=>{e.stopPropagation()
+                                      setSafeway(!safeway)}} 
+                      className={`${safeway ? "bg-blue-500 border-blue-200" : "bg-white border-gray-500" } duration-100 text-center border-2 rounded-lg px-2 py-1 cursor-pointer`}>
+                    Safeway 
+                  </div>
+                </div>
+            </div>
+          :
+            <></>
+        }
+        {
+          showBart ?
+            <div onClick={()=>setShowBart(false)}
+              className={`w-full whitespace-nowrap  rounded-lg flex items-center duration-200 mr-2 cursor-pointer`}>
+                <BsTrainFront className="w-4 h-4 mx-1"/>BART Stations
+            </div>
+          :
+            <></>
+        }
+        {
+          showGyms ?
+            <div onClick={()=>setShowGyms(false)}
+              className={`w-full whitespace-nowrap  rounded-lg flex flex-col duration-200 mr-2 cursor-pointer`}>
+               
+               <div className="flex items-center">
+                <AiOutlineShoppingCart className="w-4 h-4 mx-1"/>Grocery Chains
+                </div>
+               <div className="grid grid-cols-3 gap-2">
+                  <div onClick={(e)=>{e.stopPropagation()
+                                      setTraderJoes(!traderJoes)}} 
+                      className={`${traderJoes ? "bg-blue-500 border-blue-200" : "bg-white border-gray-500" } duration-100 text-center border-2 rounded-lg px-2 py-1 cursor-pointer`}>
+                    Trader Joe's
+                  </div>
+                  <div onClick={(e)=>{e.stopPropagation()
+                                      setWholeFoods(!wholeFoods)}} 
+                      className={`${wholeFoods ? "bg-blue-500 border-blue-200" : "bg-white border-gray-500" } duration-100 text-center border-2 rounded-lg px-2 py-1 cursor-pointer`}>
+                    Whole Foods
+                  </div>
+                  <div onClick={(e)=>{e.stopPropagation()
+                                      setSafeway(!safeway)}} 
+                      className={`${safeway ? "bg-blue-500 border-blue-200" : "bg-white border-gray-500" } duration-100 text-center border-2 rounded-lg px-2 py-1 cursor-pointer`}>
+                    Safeway 
+                  </div>
+                </div>
+            </div>
+          :
+            <></>
+        }
+        {
+          showBuses ?
+            <div onClick={()=>setShowBuses(false)}
+              className={`w-full whitespace-nowrap  rounded-lg flex items-center duration-200 mr-2 cursor-pointer`}>
+                <LiaBusSolid className="w-4 h-4 mx-1"/>Bus Stops
+            </div>
+          :
+            <></>
+        }
+        {
+          showBikes ?
+            <div onClick={()=>setShowBikes(false)}
+              className={`w-full whitespace-nowrap  rounded-lg flex items-center duration-200 mr-2 cursor-pointer`}>
+                <PiPersonSimpleBikeBold className="w-4 h-4 mx-1"/>BikeShare
+            </div>
+          :
+            <></>
+        }
+
+
+    </div>
+    )
+  }
 };
 
 export default Home;
